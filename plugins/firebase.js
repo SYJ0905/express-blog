5988806bca5848ad927b4e10186823f4d9d89b1b
Cloud <BetaPhoenixSNSD@gmail.com>
2020-01-15 09:46:42 +0800

    連結 firebase 模組

---
+++ plugins/firebase.js
@@ -0,0 +1,15 @@
+const firebase = require('firebase');
+require('dotenv').config();
+
+firebase.initializeApp({
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASEURL,
+  projectId: process.env.FIREBASE_PROJECTID,
+  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.FIREBASE_APPID,
+  measurementId: process.env.FIREBASE_MEASUREMENTID,
+});
+
+module.exports = firebase;
