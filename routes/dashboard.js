7bec61c9c219ed4c983a46584a837ee77c9320ef
Cloud <BetaPhoenixSNSD@gmail.com>
2020-01-16 17:01:19 +0800

    新增-建立文章page API+特定文章page API+更新文章 API

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,23 +4,99 @@ const firebaseAdmin = require('../plugins/firebase-admin');
 const router = express.Router();
 
 const categoriesRef = firebaseAdmin.ref('/categories/');
+const articlesRef = firebaseAdmin.ref('/articles/');
 
 /* GET dashboard/archives page. */
 router.get('/archives', (req, res) => {
   res.render('dashboard/archives', { title: 'Express' });
 });
 
-/* GET dashboard/article page. */
-router.get('/article', (req, res) => {
-  res.render('dashboard/article', { title: 'Express' });
+/* GET /article/create 新增文章頁面 */
+router.get('/article/create', (req, res) => {
+  categoriesRef.once('value')
+    .then((dataSnapshot) => {
+      const categories = dataSnapshot.val();
+      const article = {};
+      res.render('dashboard/article', {
+        title: 'Express',
+        categories,
+        article,
+      });
+    })
+    .catch((error) => {
+      console.log('渲染新增文章頁面失敗', error.message);
+      res.redirect('/');
+    });
 });
 
-/* GET dashboard/categories page. */
+/* GET /article/:id 特定文章頁面 */
+router.get('/article/:id', (req, res) => {
+  const id = req.params.id;
+  let categories = {};
+  categoriesRef.once('value')
+    .then((dataSnapshot) => {
+      categories = dataSnapshot.val();
+      return articlesRef.child(id).once('value');
+    })
+    .then((dataSnapshot) => {
+      const article = dataSnapshot.val();
+      res.render('dashboard/article', {
+        title: 'Express',
+        categories,
+        article,
+      });
+    })
+    .catch((error) => {
+      console.log('取得特定文章頁面失敗', error.message);
+    });
+});
+
+
+/* POST API /article/create 新增文章 */
+router.post('/article/create', (req, res) => {
+  const articleRef = articlesRef.push();
+  const data = {
+    title: req.body.title,
+    content: req.body.content,
+    category: req.body.category,
+    status: req.body.status,
+    id: articleRef.key,
+    update_time: Math.floor(Date.now() / 1000),
+  };
+  articleRef.set(data)
+    .then(() => {
+      res.redirect(`/dashboard/article/${data.id}`);
+    })
+    .catch((error) => {
+      console.log('新增文章失敗', error.message);
+    });
+});
+
+/* POST API /article/update/:id 更新文章 */
+router.post('/article/update/:id', (req, res) => {
+  const id = req.params.id;
+  const data = {
+    title: req.body.title,
+    content: req.body.content,
+    category: req.body.category,
+    status: req.body.status,
+    id,
+    update_time: Math.floor(Date.now() / 1000),
+  };
+  articlesRef.child(id).update(data)
+    .then(() => {
+      res.redirect(`/dashboard/article/${id}`);
+    })
+    .catch((error) => {
+      console.log('更新特定文章失敗', error.message);
+    });
+});
+
+/* GET /dashboard/categories 文章分類頁面 */
 router.get('/categories', (req, res) => {
   const message = req.flash('info');
   categoriesRef.once('value')
     .then((dataSnapshot) => {
-      console.log('取得文章分類成功');
       const categories = dataSnapshot.val();
       res.render('dashboard/categories', {
         title: 'Express',
@@ -35,7 +111,7 @@ router.get('/categories', (req, res) => {
     });
 });
 
-// 新增文章分類 API
+/* POST API /categories/create 新增文章分類 */
 router.post('/categories/create', (req, res) => {
   const categoryRef = categoriesRef.push();
   const key = categoryRef.key;
@@ -52,7 +128,6 @@ router.post('/categories/create', (req, res) => {
       } else {
         categoryRef.set(data)
           .then(() => {
-            console.log('新增文章分類成功');
             res.redirect('/dashboard/categories');
           })
           .catch((error) => {
@@ -63,12 +138,11 @@ router.post('/categories/create', (req, res) => {
     });
 });
 
-// 刪除文章分類 API
+/* POST API /categories/delete/:id 刪除文章分類 */
 router.post('/categories/delete/:id', (req, res) => {
   const id = req.params.id;
   categoriesRef.child(id).remove()
     .then(() => {
-      console.log('刪除文章分類成功');
       req.flash('info', '欄位已刪除');
       res.redirect('/dashboard/categories');
     })
